d60c0e0f56ded4455ca2b7b8d18429a4329d27e0
NC12345 <cruznicoj@gmail.com>
2025-10-07 14:29:42 -0700

    Add RelativePosition wrapper and FlattenObservation wrapper

--- run_gymnasium_env.py
+++ run_gymnasium_env.py
@@ -1,12 +1,18 @@
 import gymnasium
-import gymnasium_env  # <- triggers registration
+import gymnasium_env
+from gymnasium.wrappers import FlattenObservation
+from gymnasium_env.wrappers import RelativePosition
+
 
 env = gymnasium.make("gymnasium_env/GridWorld-v0", render_mode="human",size=5)
+# env = FlattenObservation(env)
+env = RelativePosition(env)
 obs, info = env.reset()
 
 for _ in range(30):
     action = env.action_space.sample()  # move randomly
     obs, reward, terminated, truncated, info = env.step(action)
+    print(f"Step {_}: obs = {obs}, reward = {reward}")
     if terminated or truncated:
         obs, info = env.reset()
 
